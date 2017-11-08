d73b2aedc38a239ce80095708891aa67c5e9ec51
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2017-11-08 02:42:30 -0700

    added more examples

--- index.js
+++ index.js
@@ -63,3 +63,124 @@ console.log(
 prints
 Pirates Of The Caribbean
 */
+
+console.log(
+    [1, 2, 3, 4, 5, 6]
+        .filter(x => x % 2 === 0))
+/*
+prints
+[ 2, 4, 6 ]
+*/
+
+console.log(
+    [{name: 'bob'},
+     {name: 'anonymous'},
+     {name: 'jane'},
+     {name: 'anonymous'},
+     {name: 'jonjacobjingleheimerschmidt'}]
+        .filter(user => user.name !== 'anonymous'))
+/*
+prints
+[ { name: 'bob' },
+  { name: 'jane' },
+  { name: 'jonjacobjingleheimerschmidt' } ]
+*/
+
+console.log(
+    [1, 2, 3, 4, 5]
+        .reduce(
+            (acc, x) => acc + x,
+            0))
+/*
+prints
+15
+*/
+
+const
+    map = (list, f) =>
+        list.reduce((acc, x) => acc.concat([f(x)]), [])
+
+console.log(map([1, 2, 3], x => x * 2))
+/*
+prints
+[ 2, 4, 6 ]
+*/
+
+const
+    filter = (list, f) =>
+        list.reduce(
+            (acc, x) => f(x) ? acc.concat([x]) : acc,
+            [])
+
+console.log(filter([1, 2, 3, 4], x => x % 2 === 0))
+/*
+prints
+[ 2, 4 ]
+*/
+
+const
+    getResults = () => [
+            {
+                results: [
+                    {
+                        name: 'joe',
+                        occupation: 'space whale cleaner',
+                    }
+                ],
+            },
+            {
+                results: [
+                    {
+                        name: 'jane',
+                        occupation: 'interdimensional detective',
+                    }
+                ],
+            },
+            {
+                results: [
+                    {
+                        name: 'strongbad',
+                        occupation: 'space whale cleaner',
+                    }
+                ],
+            },
+            {
+                results: [
+                ],
+            },
+            {
+                results: [
+                    {
+                        name: 'wil',
+                        occupation: 'starship captain',
+                    }
+                ],
+            },
+            {
+                results: [
+                    {
+                        name: 'kathryn',
+                        occupation: 'starship captain',
+                    }
+                ],
+            },
+        ],
+    set = (obj, key, value) => Object.assign({}, obj, {[key]: value})
+
+console.log(
+    getResults()
+        .map(queryResult => queryResult.results[0])
+        .filter(result => result)
+        .map(result => result.occupation)
+        .reduce(
+                (counts, occupation) =>
+                    counts[occupation]
+                        ? set(counts, occupation, counts[occupation] + 1)
+                        : set(counts, occupation, 1),
+                {}))
+/*
+prints
+{ 'space whale cleaner': 2,
+  'interdimensional detective': 1,
+  'starship captain': 2 }
+*/
